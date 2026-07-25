efa71724f5c892f878163de65d0263ae16ad8af8
Shams <shamsalam0341@gmail.com>
2026-07-26 00:07:47 +0530

    Final submission

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { loginAdmin,createAdmin } = require("../controllers/authController");
+const { loginAdmin,createAdmin } = require("../controllers/temp");
 
 router.post("/login", loginAdmin);
 // TEMPORARY - DELETE AFTER CREATING ADMIN
